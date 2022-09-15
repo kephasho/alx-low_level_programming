9944acf49cfb7e9fa9140268a55dcc2a0f98356f
“kephasho” <denniskephasho@gmail.com>
2022-09-15 13:04:14 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include"main.h"
+#include "main.h"
 
 /**
-* main - Entry point
-*
-* Description: prints _putchar
-*
-* Return: 0
-*/
-
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-const char *to_print = "_putchar";
-long unsigned int i;
-for (i = 0; i < strlen(to_print); ++i)
-{
-  putchar(to_print[i]);
-}
-putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
